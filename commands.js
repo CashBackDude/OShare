bc9dd0d366f129b38c4bcebc68bd4667313a4301
Jorge <102768278+CashBackDude@users.noreply.github.com>
2025-11-04 08:53:21 -0500

    Refactor commands.js with new action function
    Testing

--- commands.js
+++ commands.js
@@ -1,15 +1,47 @@
-// Copyright (c) Microsoft Corporation. Licensed under the MIT license.
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global global, Office, self, window */
 
 Office.onReady(() => {
-  // Office.js is ready
+  // If needed, Office.js is ready to be called
 });
 
-function openForm(event) {
-  window.open("https://forms.office.com/e/0WMwRUR02J"); // Target URL
+/**
+ * Shows a notification when the add-in command is executed.
+ * @param event {Office.AddinCommands.Event}
+ */
+function action(event) {
+  const message = {
+    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+    message: "Opened Microsoft 365 end-user quarantine in browser.",
+    icon: "Icon.80x80",
+    persistent: true,
+  };
+
+  // Open end-user quarantine in a browser window
+  window.open("https://security.microsoft.com/quarantine?viewid=Email");
+
+  // Show a notification message
+  // Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+
+  // Be sure to indicate when the add-in command function is complete
   event.completed();
 }
 
-// Make function globally accessible
-if (typeof window !== "undefined") {
-  window.openForm = openForm;
+function getGlobal() {
+  return typeof self !== "undefined"
+    ? self
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : undefined;
 }
+
+const g = getGlobal();
+
+// The add-in command functions need to be available in global scope
+g.action = action;
